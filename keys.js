5fbbd358491df50c0d1c2a506dad351212561bc4
Nate Price <nate.e.price@gmail.com>
2019-10-05 16:57:46 -0500

    liri is working

---
+++ keys.js
@@ -0,0 +1,15 @@
+module.exports = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+}
+
+// module.exports = {
+//   spotify,
+// }
+
+// module.exports = {
+//   spotify: {
+//     id: process.env.SPOTIFY_ID,
+//     secret: process.env.SPOTIFY_SECRET
+//   }
+// }
